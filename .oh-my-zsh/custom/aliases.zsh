1a66f53352b09b89a8fedc8be02dec4e3d83700b
voltamage <37029882+voltamage@users.noreply.github.com>
2020-04-20 20:58:07 -0700

    Update aliases.zsh

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,3 @@
-alias backport="dpkg -l | grep '~bpo'"
 alias crsync='sudo rsync --acls --archive --partial --progress --recursive --stats -vv --xattrs'
 alias hostsupdate='bash -c "$(curl -fsSL https://raw.githubusercontent.com/voltamage/common/master/tmp/hosts_updater)"'
 alias iplist='nmap -sP 192.168.2.1/24'
